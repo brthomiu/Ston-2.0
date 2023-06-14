ededb64a680e1dce0c8350f82489bd375be99e1b
Brad <80889504+brthomiu@users.noreply.github.com>
2023-06-14 11:56:15 -0600

    STON-13 Setup frontend for automated testing with Vite/React/Vitest

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTest.ts",
+    // speed up since test don't rely on css
+    css: false,
+  }
+})
