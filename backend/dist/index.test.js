7f498c0df1f69ee1f2910414b5e22ac140261353
Brad <80889504+brthomiu@users.noreply.github.com>
2023-06-17 18:27:10 -0600

    STON-2 Add unit test for database connection

--- backend/dist/index.test.js
+++ backend/dist/index.test.js
@@ -1,7 +1,35 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
 const vitest_1 = require("vitest");
-// Sample test to make sure vitest is working
-(0, vitest_1.test)('should execute', () => {
-    (0, vitest_1.expect)(1 + 1).toBe(2);
+const connectToDb_1 = __importDefault(require("./utils/connectToDb"));
+(0, vitest_1.describe)('Server Connection to MongoDB', () => {
+    (0, vitest_1.beforeAll)(() => {
+        // Connect to a mock MongoDB server
+        const mongoUri = 'mongodb://localhost:27017/testdb';
+        mongoose_1.default.connect = vitest_1.vi.fn().mockResolvedValue(undefined); // Mock the connect function
+        process.env.TEST_DB_URI = mongoUri;
+    });
+    (0, vitest_1.afterAll)(() => {
+        // Disconnect from the mock MongoDB server
+        mongoose_1.default.disconnect();
+    });
+    (0, vitest_1.it)('should connect to the database', () => __awaiter(void 0, void 0, void 0, function* () {
+        // Call the connectToDb function
+        yield (0, connectToDb_1.default)();
+        // Expect the mongoose.connect function to have been called with the correct URI
+        (0, vitest_1.expect)(mongoose_1.default.connect).toHaveBeenCalledWith(process.env.TEST_DB_URI);
+    }));
 });
--- backend/src/index.test.ts
+++ backend/src/index.test.ts
@@ -1,6 +1,26 @@
-import { test, expect } from 'vitest';
+import mongoose from 'mongoose';
+import { beforeAll, afterAll, describe, expect, it, vi } from 'vitest';
+import connectToDb from './utils/connectToDb';
 
-// Sample test to make sure vitest is working
-test('should execute', () => {
-  expect(1 + 1).toBe(2);
+describe('Server Connection to MongoDB', () => {
+  beforeAll(() => {
+    // Connect to a mock MongoDB server
+    const mongoUri = 'mongodb://localhost:27017/testdb';
+    mongoose.connect = vi.fn().mockResolvedValue(undefined); // Mock the connect function
+
+    process.env.TEST_DB_URI = mongoUri;
+  });
+
+  afterAll(() => {
+    // Disconnect from the mock MongoDB server
+    mongoose.disconnect();
+  });
+
+  it('should connect to the database', async () => {
+    // Call the connectToDb function
+    await connectToDb();
+
+    // Expect the mongoose.connect function to have been called with the correct URI
+    expect(mongoose.connect).toHaveBeenCalledWith(process.env.TEST_DB_URI);
+  });
 });
